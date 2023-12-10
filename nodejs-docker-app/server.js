2bc747300ffc07102e7a06131a501745d23a5e5a
hugo <werwer748@naver.com>
2023-12-10 18:48:52 +0900

    천천히 따라가면 어렵지는 않음

---
+++ nodejs-docker-app/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = 8080;
+
+//APP
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("안녕하세요~");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
